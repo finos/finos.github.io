1ada8e222a38b741027c210e1cd9deb51fa62009
Maurizio Pillitu <maoo@apache.org>
2018-05-14 00:04:35 +0200

    :ambulance: testing selenium error

--- tests/test-rootpath.js
+++ tests/test-rootpath.js
@@ -4,17 +4,18 @@ var assert = require('assert');
 
 var driver = new webdriver.Builder().forBrowser("chrome").build();
  
-driver.get('http://localhost:8080/?sort=hotness-down');
+// driver.get('http://localhost:8080/?sort=hotness-down');
 
-// driver.get('http://localhost:8080/?sort=hotness-down').then(function () {
-// 	console.log("title is " + driver.getTitle());
-//    	return driver.getTitle();
-// });
-
-driver.getTitle().then(function(title) {
-  console.log("title is " + title);
-  assert(title.toLowerCase().indexOf("FINOS Catalogu")!==1);
+driver.get('http://localhost:8080/?sort=hotness-down').then(function () {
+	console.log("title is " + driver.getTitle());
+	assert(driver.getTitle().toLowerCase().indexOf("FINOS Catalogu")!==1);
+   	return driver.getTitle();
 });
+
+// driver.getTitle().then(function(title) {
+//   console.log("title is " + title);
+//   assert(title.toLowerCase().indexOf("FINOS Catalogu")!==1);
+// });
 
 // driver.findElement(By.id('activity-recap'))
 //   .getText().then(textValue => {
