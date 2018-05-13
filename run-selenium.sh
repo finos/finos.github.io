10743e21dc3842cf6262a8ac7603170ca6654c3b
Maurizio Pillitu <maoo@apache.org>
2018-05-13 23:08:07 +0200

    :new: first shot at automating testing of FINOS Catalogue with Selenium. Still not working, but getting close

---
+++ run-selenium.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# To run it locally (on mac), use:
+# export PATH=$PATH:$PWD/selenium-server ; export CHROMEDRIVER_FILE="chromedriver_mac64.zip" ; ./run-selenium.sh
+
+if [ -z "$SELENIUM_MAJOR_VERSION" ]; then
+	export SELENIUM_MAJOR_VERSION=3.12
+fi
+
+if [ -z "$SELENIUM_VERSION" ]; then
+	export SELENIUM_VERSION=3.12.0
+fi
+
+if [ -z "$CHROMEDRIVER_VERSION" ]; then
+	export CHROMEDRIVER_VERSION=2.38
+fi
+
+if [ -z "$CHROMEDRIVER_FILE" ]; then
+	export CHROMEDRIVER_FILE="chromedriver_linux64.zip"
+fi
+
+rm -rf build/selenium-server ; mkdir -p build/selenium-server ; cd build/selenium-server
+
+wget -N http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/${CHROMEDRIVER_FILE}
+unzip ${CHROMEDRIVER_FILE}
+chmod +x chromedriver
+
+if [ -n "$TRAVIS_BUILD" ]; then
+	sudo mv -f ~/chromedriver /usr/local/share/
+	sudo chmod +x /usr/local/share/chromedriver
+	sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+fi
+
+wget http://selenium-release.storage.googleapis.com/${SELENIUM_MAJOR_VERSION}/selenium-server-standalone-${SELENIUM_VERSION}.jar
+
+java -jar selenium-server-standalone-${SELENIUM_VERSION}.jar -port 4444 > /dev/null &
+
+# make build/fonts
+# node server.js > /dev/null &
---
+++ tests/test-rootpath.js
@@ -0,0 +1,20 @@
+var webdriver = require('selenium-webdriver'), driver;
+var By = webdriver.By;
+// var assert = require('assert');
+
+var driver = new webdriver.Builder().forBrowser("chrome").build();
+ 
+driver.get('http://localhost:8080/?sort=hotness-down');
+
+driver.getTitle().then(function(title) {
+  console.log("title is " + title);
+  // assert(title.toLowerCase().indexOf("selenium test page")!==1);
+});
+
+driver.findElement(By.id('activity-recap'))
+  .getText().then(textValue => {
+    console.log("recap is " + textValue);
+    // assert.equal('(all)', textValue);
+  });
+ 
+driver.quit();
